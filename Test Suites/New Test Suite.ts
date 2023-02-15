6935935693ffbc9677069c6ce2fa0f8f7c4ccf2d
yooguu <yogadwipriyatmoko@gmail.com>
2023-02-16 00:41:50 +0700

    test update

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>854b00b6-e580-4bd0-b42d-397ab58f4cd1</testSuiteGuid>
+</TestSuiteEntity>
